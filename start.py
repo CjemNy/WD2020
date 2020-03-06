a8a3aecb8acf7654f64efe6f5f7784ac29ba95ce
Daniel <dsa@dsada.pl>
2020-03-06 19:06:24 +0100

    y

--- start.py
+++ start.py
@@ -1,1 +1,8 @@
-print("Helloo")
+a,b=444,666
+print(a, b)
+a = u"Helloł %(a)d" %{'a': b}
+print(a)
+lista=['asas','as','asda',556,43.7]
+print(lista)
+slownik = {'a': b}
+print(slownik)
---
+++ zad1.py
@@ -0,0 +1,27 @@
+#zadanie1
+a = 2
+x = 5.9
+b = "Hello"
+y = "Hej"
+print(a)
+print(b)
+print(x)
+print(y)
+#zadanie2
+a=4
+b=8
+c=3
+dzielenie=a/b
+print(dzielenie)
+dzielenie=a/c
+print(dzielenie)
+dodawanie=a+b
+print(dodawanie)
+dzielenie_calkowite=a//c
+print(dzielenie_calkowite)
+reszta=a%c
+print(reszta)
+potega=b**c
+print(potega)
+potega=pow(8,3)
+print(potega)
